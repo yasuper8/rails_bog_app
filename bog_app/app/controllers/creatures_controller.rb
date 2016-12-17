9e7d127eaf15f612d7041e3edddb96f0794fd705
yasuper8 <ysuper8@gmail.com>
2016-12-16 17:46:01 -0800

    finished part III

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -42,4 +42,36 @@ class CreaturesController < ApplicationController
     # remember the default behavior is to render :show
   end
 
+  def edit
+    # get the creature id from the url params
+    creature_id = params[:id]
+    # use `creature_id` to find the creature in the database
+    # and save it to an instance variable
+    @creature = Creature.find_by_id(creature_id)
+    # render the edit view (it has access to instance variable)
+    # remember the default behavior is to render :edit
+  end
+
+  def update
+    # get the creature id from the url params
+    creature_id = params[:id]
+
+    # use `creature_id` to find the creature in the database
+    creature  = Creature.find_by_id(creature_id)
+    # whitelist params and save them to a variable
+    # creature_params = params.require(:creature).permit(:name, :description)
+     # update the creature
+    creature.update_attributes(creature_params)
+    # redirect to show page for the updated creature
+    redirect_to creature_path(creature)
+    # redirect_to creature_path(creature) is equivalent to:
+    # redirect_to "/creatures/#{creature.id}"
+  end
+
+
+  def creature_params
+    # whitelist params return whitelisted version
+    params.require(:creature).permit(:name, :description)
+  end
+
 end
--- bog_app/config/routes.rb
+++ bog_app/config/routes.rb
@@ -6,9 +6,16 @@ Rails.application.routes.draw do
   get "/creatures/new", to: "creatures#new", as: "new_creature"
   post "/creatures", to: "creatures#create"
   get "/creatures/:id", to: "creatures#show", as: "creature"
+  get "/creatures/:id/edit", to: "creatures#edit", as: "edit_creature"
+  patch "/creatures/:id", to: "creatures#update"
 end
 
 
-# Prefix Verb URI Pattern          Controller#Action
-#     root GET  /                    creatures#index
-# creatures GET  /creatures(.:format) creatrues#index
+# Prefix Verb  URI Pattern                   Controller#Action
+#        root GET   /                             creatures#index
+#   creatures GET   /creatures(.:format)          creatures#index
+# new_creature GET   /creatures/new(.:format)      creatures#new
+#             POST  /creatures(.:format)          creatures#create
+#    creature GET   /creatures/:id(.:format)      creatures#show
+# edit_creature GET   /creatures/:id/edit(.:format) creatures#edit
+#             PATCH /creatures/:id(.:format)      creatures#update
