ca77162348dfb26f8955cac21de5aa06f515aa1a
yasuper8 <ysuper8@gmail.com>
2016-12-18 15:01:45 -0800

    finished third-run. time 39 min

---
+++ bog_app_3/app/controllers/creatures_controller.rb
@@ -0,0 +1,66 @@
+class CreaturesController < ApplicationController
+
+
+  def index
+    @creatures = Creature.all
+  end
+
+
+  def new
+    @creature = Creature.new
+  end
+
+  def create
+    creature_params = params.require(:creature).permit(:neam, :description)
+
+    creature = Creature.new(creature_params)
+
+    if creature.save
+      redirect_to creature_path(creature)
+    end
+  end
+
+  def show
+    creature_id = params[:id]
+
+    @creature = Creature.find_by_id(creature_id)
+  end
+
+  def edit
+    creature_id = params[:id]
+
+    @creature = Creature.find_by_id(creature_id)
+
+  end
+
+
+  def update
+    creature_id = params[:id]
+
+    creature = Creature.find_by_id(creature_id)
+
+    creature.update_attributes(creature_params)
+
+    redirect_to creature_path(creature)
+
+  end
+
+
+  def destroy
+    creature_id = params[:id]
+
+    creature = Creature.find_by_id(creature_id)
+
+    creature.destroy
+
+    redirect_to creatures_path
+
+  end
+
+  private
+
+  def creature_params
+    params.require(:creature).permit(:neam, :description)
+  end
+
+end
